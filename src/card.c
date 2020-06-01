31b2f45bc86237f5443adf628c90e75b6b8bfa8f
devadmin <devadmin@mint-mate-193-x64>
2020-06-01 09:40:34 -0500

    Addressed suit and face display.

--- src/card.c
+++ src/card.c
@@ -77,10 +77,13 @@ bool card_equals(Card *card, void *obj)
 
 wchar_t* card_tostring(Card *card)
 {
-	int ARRAY_LENGTH = 3;
+	int ARRAY_LENGTH = 5;
 	wchar_t *buffer = malloc( sizeof(wchar_t) * ARRAY_LENGTH);
 	memset(buffer, '\0', sizeof(char) * ARRAY_LENGTH);
-	wchar_t *s = L"\u2663";
+	wchar_t *s = malloc( sizeof(wchar_t) * 2);
+	wchar_t *f = malloc( sizeof(wchar_t) * 3);
+	memset(f, '\0', sizeof(char) * 3);
+
 	switch (card->suit)
 	{
 		case Spade:
@@ -93,10 +96,30 @@ wchar_t* card_tostring(Card *card)
 			s = L"\u2666";
 			break;
 		default:
+			s = L"\u2663";
 			break;
 	}
 
-	swprintf(buffer, ARRAY_LENGTH, L"%d%ls", card->value, s);
+	switch (card->face)
+	{
+		case Jack:
+			swprintf(f, 3, L"%ls", L"J");
+			break;
+		case Queen:
+			swprintf(f, 3, L"%ls", L"Q");
+			break;
+		case King:
+			swprintf(f, 3, L"%ls", L"K");
+			break;
+		case Ace:
+			swprintf(f, 3, L"%ls", L"A");
+			break;
+		default:
+			swprintf(f, 3, L"%d", card->value);
+			break;
+	}
+
+	swprintf(buffer, ARRAY_LENGTH, L"%ls%ls", f, s);
 
 	return buffer;
 }
